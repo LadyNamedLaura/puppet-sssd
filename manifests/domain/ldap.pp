41d394898a352790930e7b97c1d5a7a1260418ec
Simon Peeters <simon@inuits.eu>
2016-06-07 17:54:10 +0200

    add default bind parameters

--- manifests/domain/ldap.pp
+++ manifests/domain/ldap.pp
@@ -3,6 +3,9 @@
 define sssd::domain::ldap (
   $uri,
   $search_base,
+  $authtok_type    = undef,
+  $authtok         = undef,
+  $bind_dn         = undef,
   $schema          = 'rfc2307',
   $start_tls       = true,
   $tls_reqcert     = 'demand',
@@ -14,19 +17,21 @@ define sssd::domain::ldap (
 ) {
 
   $options = {
-    id_provider           => 'ldap',
-    auth_provider         => 'ldap',
-    chpass_provider       => 'ldap',
-    cache_credentials     => true,
-    entry_cache_timeout   => $cache_timeout,
-    ldap_uri              => $uri,
-    ldap_schame           => $schema,
-    ldap_search_base      => $search_base,
-    ldap_id_use_start_tls => $start_tls,
-    ldap_tls_reqcert      => $tls_reqcert,
-    ldap_tls_cacert       => $tls_cacert,
-    ldap_network_timeout  => $network_timeout,
-
+    id_provider               => 'ldap',
+    auth_provider             => 'ldap',
+    chpass_provider           => 'ldap',
+    cache_credentials         => true,
+    entry_cache_timeout       => $cache_timeout,
+    ldap_uri                  => $uri,
+    ldap_schame               => $schema,
+    ldap_search_base          => $search_base,
+    ldap_id_use_start_tls     => $start_tls,
+    ldap_tls_reqcert          => $tls_reqcert,
+    ldap_tls_cacert           => $tls_cacert,
+    ldap_default_authtok_type => $authtok_type,
+    ldap_default_authtok      => $authtok,
+    ldap_default_bind_dn      => $bind_dn,
+    ldap_network_timeout      => $network_timeout,
   }
   sssd::domain{$name:
     options => merge($options,$extra_otions)
